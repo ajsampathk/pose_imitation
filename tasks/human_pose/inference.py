fc1432486954a21dec7f8834763272af6319f242
AJ <aj.sampathk@gmail.com>
2019-12-29 16:04:25 +0530

    Tested ROS basic

--- tasks/human_pose/inference.py
+++ tasks/human_pose/inference.py
@@ -1,4 +1,4 @@
-print("Loading Modules....",end=' ')
+print("Loading TRT Modules....")
 import json
 import trt_pose.coco
 import trt_pose.models
@@ -9,10 +9,12 @@ import cv2
 import torchvision.transforms as transforms
 import PIL.Image
 from jetcam.usb_camera import USBCamera
-from jetcam.utils import bgr8_to_jpeg
+
 
 from trt_pose.draw_objects import DrawObjects
 from trt_pose.parse_objects import ParseObjects
+
+
 
 OPTIMIZED_MODEL = 'models/resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
 
@@ -23,6 +25,18 @@ with open('human_pose.json', 'r') as f:
 topology = trt_pose.coco.coco_category_to_topology(human_pose)
 
 print("Done")
+
+print("Loading ROS Modules....")
+import rospy
+from sensor_msgs.msg import Image
+from trt_bridge.msg import *
+human_msg = Human()
+
+rospy.init_node('Inference')
+image_pub = rospy.Publisher("/Inference/pre_image",Image,queue_size=5)
+human_pub = rospy.Publisher("/Inference/Humans", Human,queue_size=5)
+print("Done")
+
 num_parts = len(human_pose['keypoints'])
 num_links = len(human_pose['skeleton'])
 print("Loading Model...")
@@ -47,26 +61,44 @@ draw = DrawObjects(topology)
 camera = USBCamera(width=224, height=224, capture_fps=30)
 camera.running = True
 
+def publish_image(image):
+    img = Image()
+    (img.width,img.height,n) = image.shape
+    img.encoding = "rgb8"
+    img.is_bigendian = 0
+    img.data = image.ravel().tolist()
+    img.step = 3
+    image_pub.publish(img)
+
+def human_publish(people):
+    for i in range(len(people)):
+        human_msg.id = i
+        for key in people[i]:
+            part = keypoint()
+            part.x,part.y = people[i][key]
+            setattr(human_msg,key,part)
+        human_pub.publish(human_msg)        
+    
+
 print("Starting Inference..")
 def execute(change):
-    image = change['new']
-    data = preprocess(image)
-    cmap, paf = model_trt(data)
-    cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
-    counts, objects, peaks = parse(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
-    print(get_points(counts,objects,peaks))
-    draw(image, counts, objects, peaks)
-    cv2.imshow('image',image)
-    cv2.waitKey(1)
-
+    try:
+        image = change['new']
+        data = preprocess(image)
+        cmap, paf = model_trt(data)
+        cmap, paf = cmap.detach().cpu(), paf.detach().cpu()
+        counts, objects, peaks = parse(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
+        draw(image, counts, objects, peaks)
+        publish_image(image)
+        human_publish(get_points(counts,objects,peaks))
+        rospy.loginfo("Inference complete {} person(s) detected".format(counts[0]))
+    except KeyboardInterrupt:
+        print("Shutting Down...")
+        camera.unobserve_all()
+        exit()
 
 camera.observe(execute,names='value')
 
-try:
-    pass
-except KeyboardInterrupt:
-    print("shutting down..")
-    camera.unobserve_all()
     
 def get_points(counts,objects,peaks):
     height = 224
@@ -82,7 +114,9 @@ def get_points(counts,objects,peaks):
             if value >=0:
                 peak = peaks[0][key][value]
                 x,y = (round(float(peak[1])*width),round(float(peak[0])*height))
-                person[human_pose['keypoints'][key]]=(x,y)
+            else:
+                x,y = -1,-1
+            person[human_pose['keypoints'][key]]=(x,y)
         people.append(person)
     return people        
     
